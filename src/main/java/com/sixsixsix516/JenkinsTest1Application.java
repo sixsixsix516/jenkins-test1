3fce61585f7809911c82a33b7481ff82fdc6ba6a
Sun <sixsixsix516@163.com>
2020-09-24 14:42:42 +0800

    11

---
+++ src/main/java/com/sixsixsix516/JenkinsTest1Application.java
@@ -0,0 +1,21 @@
+package com.sixsixsix516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTest1Application.class, args);
+	}
+
+
+	@GetMapping
+	public String hello() {
+		return "Hello Jenkins!";
+	}
+}
